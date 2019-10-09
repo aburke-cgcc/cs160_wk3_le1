2419f5602de14c64aaffa3e1e2d56b2e745c992f
aburke-cgcc <55819868+aburke-cgcc@users.noreply.github.com>
2019-10-08 19:19:12 -0700

    First Upload

---
+++ src/edu/cgcc/cs160/MainEntry.java
@@ -0,0 +1,27 @@
+package edu.cgcc.cs160;
+
+public class MainEntry {
+
+	public static void main(String[] args) {
+		int i = 1;
+		double minute = 59;
+		double j = minute / 60f;
+		
+		if(i == 1 && i == 2) {
+			System.out.println(j);
+		}
+		
+		/*//declare integers for number program
+		int number = 1;
+		int sum = 50;
+		
+		//loop through and increment number by 1 until sum
+		while(number < sum) {
+			number = number + 1;
+		}
+		
+		//print number to console
+		System.out.println(number);*/
+	}
+
+}
